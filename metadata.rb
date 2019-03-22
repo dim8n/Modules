ff21a6b1ba9976a3cce2a305afb9d113f75726b4
Dzmitry_Yelizarau <46851358+dim8n@users.noreply.github.com>
2019-03-22 13:18:22 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,20 @@
 name 'task9-cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Dzmitry Yelizarau'
+maintainer_email 'dzmitry_yelizarau@epam.com'
 license 'All Rights Reserved'
 description 'Installs/Configures task9-cookbook'
 long_description 'Installs/Configures task9-cookbook'
-version '0.4.10'
+version '0.2.14'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/task9-cookbook/issues'
+# issues_url 'https://github.com/dim8n/Modules/Task9/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/task9-cookbook'
+# source_url 'https://github.com/dim8n/Modules/Task9'
