46101da28eacb79f9f963a23e0486a0bb628305c
Dzmitry_Yelizarau <46851358+dim8n@users.noreply.github.com>
2019-03-22 13:15:07 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,16 @@
 #
-# Cookbook:: task9-cookbook
+# Cookbook:: task9_cookbook
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-#file "#{ENV['HOME']}/hello.txt" do
-#  content 'This file was created by Chef and Dimon)!'
-#end
-package 'docker.io'
+
+#package 'docker.io' #for Ubuntu and Mint
 package 'docker'
 
+file "/etc/docker/daemon.json" do
+    content "{\n\t\"insecure-registries\" : [\"127.0.0.1:5000\"]\n}\n"
+end
+
+service 'docker' do
+  action [:enable, :start]
+end
