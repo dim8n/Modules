37dbed12610f4cd8054517256a31ed464883441c
Dzmitry_Yelizarau <46851358+dim8n@users.noreply.github.com>
2019-03-22 14:14:53 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,20 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 #package 'docker.io' #for Ubuntu and Mint
-package 'docker'
+
+case node[:platform]
+  when 'ubuntu','debian'
+  package 'docker.io' do
+    action :install
+  end
+end
+
+package 'docker' do
+  action :install
+end
 
 file "/etc/docker/daemon.json" do
-    content "{\n\t\"insecure-registries\" : [\"127.0.0.1:5000\"]\n}\n"
+  content "{\n\t\"insecure-registries\" : [\"127.0.0.1:5000\"]\n}\n"
 end
 
 service 'docker' do
