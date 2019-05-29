9d148fb4c237563fe71f21b084bb878536cdc134
123123123 <123123123@n93820.nbrb.by>
2019-05-29 10:53:39 +0300

    all works

--- main.tf
+++ main.tf
@@ -94,10 +94,11 @@ resource "aws_lb_listener" "TF_ALB" {
 }
 
 # auto scaling group
+
 resource "aws_autoscaling_group" "TF_auto_scaling_group" {
   name                      = "TF_auto_scaling_group"
   max_size                  = 7
-  min_size                  = 1
+  min_size                  = 0
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 7
@@ -105,4 +106,11 @@ resource "aws_autoscaling_group" "TF_auto_scaling_group" {
   target_group_arns         = ["${aws_lb_target_group.TF_target_group.arn}"]
   launch_configuration      = "${aws_launch_configuration.as_conf.name}"
   vpc_zone_identifier       = ["subnet-77cc7e3a","subnet-892e78e0","subnet-9f7f16e4"] # список зон нужно настроить брать автоматически
-}
+  tags = [
+      {
+        key                 = "Name"
+        value               = "TF_web_server"
+        propagate_at_launch = true
+      }
+    ]
+  }
